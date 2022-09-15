0753243ef8d2dd45b1bd4fa16ffd3705f9b4e8fd
Ali <ali3.1415@outlook.com>
2022-09-15 14:04:02 +0300

    I have rewritten second app with scss

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import CourseGoalItem from "./CourseGoals/CourseGoalItem/CourseGoalItem";
+import CourseGoalList from "./CourseGoals/CourseGoalList/CourseGoalList";
+import CourseInput from "./CourseGoals/CourseInput/CourseInput";
+import Button from "./UI/Button/Button";
+
+export { CourseGoalItem, CourseGoalList, CourseInput, Button };
